4e72e06117fd6045e8f6772dc08f46b65a24e6aa
Jin Bok <devJin86@gmail.com>
2016-04-07 11:15:33 -0700

    Implement basic server with some middlewares

--- server/server.js
+++ server/server.js
@@ -1,2 +1,31 @@
-//Inicial File
-//TODO: Implemet
+/* Server */
+var express = require("express");
+var path = require("path");
+var app = express();
+var port = process.env.PORT || 7777;
+
+
+/* Static */
+//
+app.use(express.static(path.join(__dirname, "../client/")));
+
+/* middlewares */
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded());
+app.set('views', path.join(__dirname, '../client/views/'));
+
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+
+
+/* Routes */
+app.get('/', function(req, res) {
+ res.render("index");
+});
+
+
+
+app.listen(8000, function() {
+ console.log("listening on port 8000");
+});
