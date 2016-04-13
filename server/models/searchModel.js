b85c5d57da76c559a6accd5593491a6997aa7928
Jovani Rico <jovanimtzrico@gmail.com>
2016-04-13 16:29:17 -0700

    Model Search and Fix Database

--- server/models/searchModel.js
+++ server/models/searchModel.js
@@ -1,11 +1,27 @@
 var db = require('../db');
 
+var NUM_ITEMS = 5;
+
 module.exports = {
   search: {
-    post: function (params, callback) {
-      var query = '';
-      db.query(query, params, function(err, results) {
+    getAllRandom: function (callback) {
+      var queryId = 'SELECT MAX(id) FROM items';
+      db.query(queryId, function(err, results) {
+        if (!err) {
+          var queryItems = 'SELECT * FROM items ORDER BY RAND() LIMIT '+ NUM_ITEMS;
+          db.query(query, params, function(err, results) {
+            callback(err, results);
+          });
+        }
+      });
+    },
+    getAllItems: function (userSearch, callback) {
+      var query = 'select i.id,i.name,i.description,i.price,i.availability from items i inner join users s on i.id = s.item_Id inner join address a on a.id  = s.address_id and a.postalcode = '+userSearch.location+' where i.name = "'+ userSearch.item+'"';
+      db.query(query, function(err, results) {
         callback(err, results);
       });
     }
   };
+
+
+// select i.id,i.name,i.description,i.price,i.availability from items i inner join users s on i.id = s.item_Id inner join address a on a.id  = s.address_id and a.postalcode = 94111;
