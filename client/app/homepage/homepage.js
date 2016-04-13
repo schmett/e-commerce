3f67794b34d922748860e2ccb6c082f26db21ada
Jin Bok <devJin86@gmail.com>
2016-04-13 16:50:48 -0700

    All homepage/dashboard page now renders based on the log-in state. If logged in, users will see checkout, logout and if not logged in, they will see signup and signin

--- client/app/homepage/homepage.js
+++ client/app/homepage/homepage.js
@@ -1,31 +1,66 @@
 var app = angular.module('e-Commer.search', []);
 
-app.controller('SearchController', function ($scope, searchFactory) {
+app.controller('SearchController', function ($scope, $window, searchFactory) {
+  
+  //get random data
+  $scope.randItemsData = searchFactory.getRandItems();
+  // this is for SEARCH FORM after LOGIN
   $scope.initData = {};
-  $scope.initData.location = 94101;
-  $scope.initData.items = 'golf club';
-  $scope.initData.dates = new Date();
-  console.log($scope.initData);
-  // console.log($scope.showBorA);
-  searchFactory.getRandItems($scope.initData);
+ 
+
+  $scope.getFormValue = function() {
+    $scope.initData.location = $scope.location;
+    $scope.initData.items = $scope.items;
+    $scope.initData.dates = $scope.dates;
+    console.log($scope.initData);
+    return searchFactory.getSearchItems($scope.initData);
+  }
+
+  $scope.formRender = function() {
+    // this is to get token and based on token val, we toggle or hide views
+    var a = $window.localStorage.getItem('com.e-Commer');
+    /*
+    Now just checking for the token from localStorage. Must be compared with db token for security issues
+     */
+    console.log(a);
+    return a ? true : false;
+  }
+  
+  // after login sending form data
+  searchFactory.getSearchItems($scope.initData);
 });
 
 app.controller('subController', function($scope) {
+  /*
+  * Problem: show or hide form based on what? token or ?
+  * solution 1: save token to db
+   */
   $scope.showBorA = true;
+
   console.log($scope.initData);
 });
 
 app.factory('searchFactory', function($http) {
   return {
-    getRandItems: function(inputValue) {
+    getSearchItems: function(inputValue) {
       return $http({
         method: 'POST',
-        url: '/api/getRandItems',
+        url: '/api/getSearchItems',
         data: inputValue
       }).then(function(res) {
         // match res.data with server side
-        res.end(res.data);
+        // res.end(res.data);
+        
+        res.end(data);
       });
+    },
+    getRandItems: function() {
+      return $http({
+        method: 'GET',
+        url: '/api/getRandItems'
+      }).then(function(res) {
+        return res.data;
+      })
     }
   }
 })
