3909ab36fe33e9a7aa4f3001e3877f1076c42b7a
Sylvia Yanakieva <sylvia.yanakieva@gmail.com>
2016-04-13 18:10:13 -0700

    Initial work on checkout html page.

--- client/app/checkout/checkout.js
+++ client/app/checkout/checkout.js
@@ -1,4 +1,22 @@
-var app = angular.module('commerce', []);
-app.controller('checkOut', function($scope) {
+//Controller for the singin user
+
+angular.module('e-Commer.checkout', [])
+
+// add arguments to the scope
+
+.controller('checkoutController', function ($scope) {
+//signin variable uses int the signin template
+$scope.checkout = {};
+
+// deleteOne
+
+// calculatetotal
+  $scope.deleteOne = function() {
+
+  }
+
+  $scope.calculateTotal = function() {}
+
 
 });
+
